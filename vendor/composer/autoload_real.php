6d5f2b3679780e33804aead2e7ccd14e6516d8e6
Rashed Kaysar <rashedkaysar321@gmail.com>
2023-07-17 22:47:12 +0600

    updated

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit782e7ebaa14b1d4be1515bab59274f1e
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit782e7ebaa14b1d4be1515bab59274f1e', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit782e7ebaa14b1d4be1515bab59274f1e', 'loadClassLoader'));
